a913ccff3c06851b5e10d733d70746d97b6078db
Denis <kachser@gmail.com>
2022-09-04 16:53:34 +0300

    square

---
+++ Desktop/GB/практика1/example1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+
+string input = Console.ReadLine();
+
+int number = int.Parse (input);
+
+int square = number * number;
+
+Console.WriteLine($"число в квадрате равно {square}");
