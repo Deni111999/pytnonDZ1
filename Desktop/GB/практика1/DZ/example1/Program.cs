0ed41df8115be0dfda5522aa7b82575f70e3b86a
Denis <kachser@gmail.com>
2022-09-04 19:51:50 +0300

    dzexamle1

---
+++ Desktop/GB/практика1/DZ/example1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите 2 числа");
+string firstNum = Console.ReadLine();
+string secondNum = Console.ReadLine();
+var num1 = double.Parse(firstNum);
+var num2 = double.Parse(secondNum);
+if (num1>num2)
+{
+    Console.WriteLine("первое число болшее а второе меньшее");
+}
+else Console.WriteLine("второе число большее а первое меньшее");
+
