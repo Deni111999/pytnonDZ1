a7c15ed76ee9fb5301b9bfeae86eb475199be6c0
Denis <kachser@gmail.com>
2022-09-04 22:45:50 +0300

    dzexamle3

---
+++ Desktop/GB/практика1/DZ/example3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите число");
+string Num = Console.ReadLine();
+var num1 = double.Parse(Num);
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("число четное");
+}
+else Console.WriteLine("число нечетное");
+
