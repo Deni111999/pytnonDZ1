f0fd87c6e3bc3389dd8c93c71c3a503ac1d18a2b
Denis <kachser@gmail.com>
2022-09-07 00:43:47 +0300

    DZ1

---
+++ Desktop/GB/практика2/DZ/example1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("ведите трехзначное число и я покажу его вторую цифру");
+string num = Console.ReadLine();
+int num1 = int.Parse(num);
+int digit2 = (num1 / 10) % 10;
+Console.Write(digit2);
