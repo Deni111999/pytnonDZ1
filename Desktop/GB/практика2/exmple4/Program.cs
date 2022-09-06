f302f9cab51126e200b8de8a2041cfe3d8decb9c
Denis <kachser@gmail.com>
2022-09-07 00:25:50 +0300

    exmple4

---
+++ Desktop/GB/практика2/exmple4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ведите  число");
+string firstNum = Console.ReadLine();
+int num1 = int.Parse(firstNum);
+if (num1 % 7 == 0 && num1 % 23 == 0)
+    Console.WriteLine("число является кратным 7 и 23");
+else
+{
+
+    Console.WriteLine("числы не кратно 7 и 23");
+}
+
