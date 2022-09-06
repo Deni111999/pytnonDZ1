a9c640c81ae7f2622b648eec9a9e6ae8b31f99a1
Denis <kachser@gmail.com>
2022-09-07 01:39:01 +0300

    DZ3

---
+++ Desktop/GB/практика2/DZ/example3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи номер дня недели");
+string firstNum = Console.ReadLine();
+int num1 = int.Parse(firstNum);
+if (num1 == 6 || num1 == 7)
+        Console.WriteLine("сегодня выходной");
+else
+        Console.WriteLine("сегодня будни");
+
+
