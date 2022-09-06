f46465f70e7e312f3fd977bfb70975a5bbd477c4
Denis <kachser@gmail.com>
2022-09-07 01:25:13 +0300

    DZ2

---
+++ Desktop/GB/практика2/DZ/example2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+string num = Convert.ToString(num1);
+if (num.Length > 2){
+  Console.WriteLine("третья цифра " + num[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
