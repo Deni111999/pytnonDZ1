fe4ada198c931174d52679638d83b2c06b1aa193
Denis <kachser@gmail.com>
2022-09-07 00:07:56 +0300

    exmple3

---
+++ Desktop/GB/практика2/exmple3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("ведите 2 числа");
+string firstNum = Console.ReadLine();
+string secondNum = Console.ReadLine();
+int num1 = int.Parse(firstNum);
+int num2 = int.Parse(secondNum);
+if (num1 % num2 == 0)
+    Console.WriteLine("числа являются кратными");
+else
+{
+    int a = (num1 % num2);
+    Console.WriteLine("числы не кратны друг другу остаток от деления ");
+    Console.Write(a);
+}
+
+
+
+
