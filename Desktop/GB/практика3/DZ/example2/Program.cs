b345ba2b461cda11231f35d3221a8f56fb315ac6
Denis <kachser@gmail.com>
2022-09-09 01:49:32 +0300

    DZ2

---
+++ Desktop/GB/практика3/DZ/example2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("введите X и Y и Z для точки A");
+string xy = Console.ReadLine()!;
+string[] parts = xy.Split(' ');
+int xA = int.Parse(parts[0]);
+int yA = int.Parse(parts[1]);
+int zA = int.Parse(parts[2]);
+
+
+Console.WriteLine("введите X и Y и Z для точки B");
+xy = Console.ReadLine()!;
+parts = xy.Split(' ');
+int xB = int.Parse(parts[0]);
+int yB = int.Parse(parts[1]);
+int zB = int.Parse(parts[2]);
+
+
+
+double distance = Math.Sqrt(Math.Pow(xA - xB, 2) + (Math.Pow(yA - yB, 2) + (Math.Pow(zA - zB, 2))));
+Console.WriteLine($"Расстояние между в пространстве  {distance:F3}");
