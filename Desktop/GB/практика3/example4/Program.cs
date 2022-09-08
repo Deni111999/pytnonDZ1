6092b4049acebbd3a156f5c756c07e91c5786ee7
Denis <kachser@gmail.com>
2022-09-09 01:29:28 +0300

    praktika3-4

---
+++ Desktop/GB/практика3/example4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+Console.WriteLine("введите число n");
+string num = Console.ReadLine();
+int n = int.Parse(num);
+int[] numbers = new int[n];
+for (int i = 1; i <= n; i++)
+    numbers[i - 1] = i * i;
+
+for (int i = 0; i < n; i++)
+    Console.WriteLine(numbers[i]);
