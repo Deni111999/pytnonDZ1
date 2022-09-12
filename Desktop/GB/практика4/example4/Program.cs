43030c9b3718e53831633c5ff761a0a8a210ac68
Denis <kachser@gmail.com>
2022-09-13 00:03:26 +0300

    p4example4

---
+++ Desktop/GB/практика4/example4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] CreateArray(int size)
+{
+    int[] numBol = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        numBol[i] = new Random().Next(0, 2);
+        
+    }
+    return numBol;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    foreach (int element in array)
+    {
+        Console.Write($"{element},");
+    }
+    Console.WriteLine("]");
+}
+PrintArray(CreateArray(8));
