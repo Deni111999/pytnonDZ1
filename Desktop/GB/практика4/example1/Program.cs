5223b86775e9960f953ede161f8c5d47b3857e6b
Denis <kachser@gmail.com>
2022-09-12 22:22:55 +0300

    p4example1

---
+++ Desktop/GB/практика4/example1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+Console.WriteLine("введите число");
+int max = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+for (int i = 1; i <= max; i++)
+    sum = sum + i;
+Console.WriteLine(sum);
