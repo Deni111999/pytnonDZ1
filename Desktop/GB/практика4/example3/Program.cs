102dc718542b9240980f3fd3cb347ebf626a8472
Denis <kachser@gmail.com>
2022-09-12 23:34:29 +0300

    p4example3

---
+++ Desktop/GB/практика4/example3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("введите число");
+int max = int.Parse(Console.ReadLine()!);
+
+int multiply = 1;
+for (int i = 1; i <= max; i++)
+    multiply = multiply * i;
+Console.WriteLine(multiply);
