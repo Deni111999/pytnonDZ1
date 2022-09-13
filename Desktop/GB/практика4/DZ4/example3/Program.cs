eb8e53633ab92e0c0a8f78ab2fb4868b7d564f39
Denis <kachser@gmail.com>
2022-09-13 15:53:23 +0300

    p4DZ3

---
+++ Desktop/GB/практика4/DZ4/example3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+int[] InputArrayElements()  
+{ 
+    Console.WriteLine("введите элементы массивы через пробел"); 
+    string elems = Console.ReadLine()!; 
+    string[] elemsSplited = elems.Split(' '); 
+    int[] array = new int[elemsSplited.Length]; 
+ 
+    for (int i = 0; i < elemsSplited.Length; i++) 
+    { 
+        array[i] = int.Parse(elemsSplited[i]); 
+    } 
+    return array; 
+} 
+void PrintArray(int[] array) 
+{ 
+    Console.Write("Ваш массив : "); 
+    foreach (int a in array) 
+    { 
+        Console.Write($" {a}, "); 
+    } 
+} 
+ 
+int[] arrayTest = InputArrayElements(); 
+PrintArray(arrayTest);
