fa95fdd65d0ad9b19a89144f9dc35787a820f69f
Denis <kachser@gmail.com>
2022-09-15 15:36:16 +0300

    p5

---
+++ Desktop/GB/практика5/example1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+const int Size = 12;
+int[] numbers = new int[Size];
+
+for (int i = 0; i < Size; i++)
+{
+    numbers[i] = new Random().Next(-9, 10);
+}
+
+int positivesSum = 0;
+int negativesSum = 0;
+
+foreach (int number in numbers)
+{
+    if (number > 0)
+        positivesSum += number;
+}
+
+foreach (int number in numbers)
+{
+     if (number < 0)
+        negativesSum += number;
+}
+Console.WriteLine($"сумма положительных={positivesSum},сумма отрицательных={negativesSum}");
