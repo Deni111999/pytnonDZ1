d473794cbb1a4bcd66447f4908a4e171e7750990
Denis <kachser@gmail.com>
2022-09-16 22:36:41 +0300

    DZ2

---
+++ Desktop/GB/практика5/DZ/example1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите размер массива");
+int Size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[Size];
+ArrayRandomNumbers(numbers);
+Console.WriteLine("рандомный массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int p = 0; p < numbers.Length; p++)
+if (numbers[p] % 2 == 0)
+count++;
+
+if (numbers.Length <5)
+Console.WriteLine($"итого {numbers.Length} числа, {count} из них чётные");
+else 
+Console.WriteLine($"итого {numbers.Length} чисел, {count} из них чётные");
+
+void ArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
---
+++ Desktop/GB/практика5/DZ/example2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позиц
+ Console.WriteLine("введите длину массива");
+int l = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[l];
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+     if(i % 2 > 0) sum = sum + array[i]; 
+}
+Console.WriteLine();
+Console.WriteLine(sum);
