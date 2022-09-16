5c353da9fc40cd092b890ec1802c4274a0afedcf
Denis <kachser@gmail.com>
2022-09-16 17:01:07 +0300

    p5-3

---
+++ Desktop/GB/практика5/example2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] numbers = CreateArray(4);
+PrintArray(numbers);
+ChangeElements(numbers);
+PrintArray(numbers);
+
+int[] CreateArray(int Size)
+{
+    int[] numbers = new int[Size];
+
+    for (int i = 0; i < Size; i++)
+    {
+        numbers[i] = new Random().Next(-100, 101);
+    }
+    return numbers;
+
+}
+
+void PrintArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number}");
+    }
+    Console.WriteLine();
+}
+
+void ChangeElements(int[] numbers)
+{   for (int i=0;i<numbers.Length;i++)
+    {
+        numbers[i]= -numbers[i];
+    } 
+}
