d37d13422682bb083c84073d989135b4c7ca8396
Denis <kachser@gmail.com>
2022-09-21 15:50:51 +0300

    DZ1-2

--- Desktop/GB/практика6/DZ/example1/Program.cs
+++ Desktop/GB/практика6/DZ/example1/Program.cs
@@ -1,13 +1,13 @@
 ﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-int CountPos (int num)
+int CountPos(int num)
 {
     int count = 0;
-    
+
     for (int i = 1; i <= num; i++)
     {
         Console.Write($"Число № {i}: ");
         int a = int.Parse(Console.ReadLine()!);
-        if( a > 0 ) count++; 
+        if (a > 0) count++;
     }
     return count;
 }
